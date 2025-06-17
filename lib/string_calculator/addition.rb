d35efae5f4c76424425bd6fc56ebc40cbb9a7c3d
Amol Joshi <amolmjoshi93@hotmail.com>
2025-06-17 15:18:31 +0000

    Create a simple String calculator with a basic method signature - Code

--- lib/string_calculator/addition.rb
+++ lib/string_calculator/addition.rb
@@ -3,6 +3,9 @@
 module StringCalculator
   class Addition
     def self.perform(numbers_str)
+      return 0 if numbers_str.nil? || numbers_str.strip.empty?
+
+      numbers_str.split(',').map(&:strip).map(&:to_i).sum
     end
   end
 end
