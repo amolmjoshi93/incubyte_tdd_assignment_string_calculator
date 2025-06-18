4b6f2bca0c0b91ef5eec1e23ae39943b64d2d929
Amol Joshi <amolmjoshi93@hotmail.com>
2025-06-18 06:19:44 +0000

    Support different delimiters and Rubocop changes

--- lib/string_calculator/addition.rb
+++ lib/string_calculator/addition.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 module StringCalculator
+  # This class is responsible for performing addition
   class Addition
     def self.perform(numbers_str)
       return 0 if numbers_str.nil? || numbers_str.strip.empty?
 
       delimiter = ','
-      # if numbers_str.starts_with?('//')
+      if numbers_str.start_with?('//')
+        delimiter_str, numbers_str = numbers_str.split("\n", 2)
+        delimiter = delimiter_str[2..]
+
+        unless numbers_str.match?(/\A(#{Regexp.escape(delimiter)}|\n|\d)+\z/)
+          raise ArgumentError, "Invalid characters found: Only digits, newline, and '#{delimiter}' allowed in custom mode"
+        end
+      end
       numbers_str.split(/#{delimiter}|\n/).map(&:strip).map(&:to_i).sum
     end
   end
--- test/test_string_calculator.rb
+++ test/test_string_calculator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class TestStringCalculator < Minitest::Test
   def test_that_it_has_a_version_number
@@ -8,7 +8,7 @@ class TestStringCalculator < Minitest::Test
   end
 
   def test_add_with_empty_string
-    assert_equal 0, StringCalculator::Addition.perform("")
+    assert_equal 0, StringCalculator::Addition.perform('')
   end
 
   def test_add_with_nil
@@ -16,15 +16,15 @@ class TestStringCalculator < Minitest::Test
   end
 
   def test_add_with_one_number
-    assert_equal 1, StringCalculator::Addition.perform("1")
+    assert_equal 1, StringCalculator::Addition.perform('1')
   end
 
   def test_add_with_multiple_numbers
-    assert_equal 22, StringCalculator::Addition.perform("1,5,7,9")
+    assert_equal 22, StringCalculator::Addition.perform('1,5,7,9')
   end
 
   def test_add_with_multiple_negative_numbers_and_spaces
-    assert_equal 11, StringCalculator::Addition.perform(" 1 , -3 , 5, 8 ")
+    assert_equal 11, StringCalculator::Addition.perform(' 1 , -3 , 5, 8 ')
   end
 
   def test_add_with_new_line_char
@@ -32,7 +32,7 @@ class TestStringCalculator < Minitest::Test
   end
 
   def test_add_with_custom_delimiter
-    assert_equal 6, StringCalculator::Addition.perform("//;\n1;2;3")
+    assert_equal 9, StringCalculator::Addition.perform("//;\n1;2;3\n3")
   end
 
   def test_add_with_custom_multi_char_delimiter
@@ -43,6 +43,6 @@ class TestStringCalculator < Minitest::Test
     error = assert_raises(ArgumentError) do
       StringCalculator::Addition.perform("//;;;\n1,2;;;3")
     end
-    assert_match /Invalid characters/, error.message
+    assert_match(/Invalid characters/, error.message)
   end
 end
